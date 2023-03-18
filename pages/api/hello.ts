1d3d988d61a684ecb7b5d28fdf21d6662bf1b054
jinyuzhao <zhaojinyu@huohua.cn>
2023-03-18 09:04:53 +0800

    关掉edge

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export const config = {
-  runtime: 'edge',
-}
+// export const config = {
+//   runtime: 'edge',
+// }
 
 type Data = {
   name: string
